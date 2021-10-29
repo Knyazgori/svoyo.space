41bcddb6ed5b92dbf55ac56316cae815b0933e76
Igor <ikkniazev@yandex.ru>
2021-10-29 12:03:12 +0300

    LOGO__LOGO_DESCR

--- client/src/components/TetrisNav/TetrisNav.jsx
+++ client/src/components/TetrisNav/TetrisNav.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import styles from './TetrisNav.module.css'
 
+import '../../index.css';
+
 
 function TetrisNav(props) {
   return (
     <>
-      <div className={styles.headerBlock}>
-
+      <div className="container">
+        <div className={styles.headerBlock}>
+          <h1 className={styles.logo}>своё</h1>
+          <p className={styles.logo_descr}>Мы — содружество людей, которые делают хорошие<br/>вещи о России и для России, потому что ее любят</p>
+        </div>
       </div>
     </> 
     
