3eb1e93ef695b6628c1b885c74756f0afbeff1bc
Igor <ikkniazev@yandex.ru>
2021-10-30 18:58:01 +0300

    LAYOUT_ABOUT_US_IS_DONE

---
+++ client/src/components/PhotoBlock/PhotoBlock.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import styles from './PhotoBlock.module.css'
+
+function PhotoBlock(props) {
+  return (
+    <>
+    <div className={styles.peopleBlock}>
+      <div className={styles.card}>
+        <div className={styles.card_img}></div>
+        <p className={styles.card_text}>Сережа Клещев<br />Всему голова</p>
+      </div>
+      <div className={styles.card}>
+        <div className={styles.card_img}></div>
+        <p className={styles.card_text}>Адель Галимова<br />зачинатель всея своё</p>
+      </div>
+      <div className={styles.card}>
+        <div className={styles.card_img}></div>
+        <p className={styles.card_text}>Катя Курмышкина<br />зачинатель всея своё</p>
+      </div>
+      <div className={styles.card}>
+        <div className={styles.card_img}></div>
+        <p className={styles.card_text}>Маша Козловцева<br />зачинатель всея своё</p>
+      </div>
+    </div>
+    <div className={styles.big_card}>
+      <div className={styles.big_card_img}></div>
+      <div className={styles.block_text}>
+        <p className={styles.big_card_text}>Содружество своё!</p>
+      </div>
+    </div>
+    </>
+  );
+}
+
+export default PhotoBlock;
