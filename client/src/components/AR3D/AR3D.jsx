8159ba2d4e8757185a31195eabb7d91756957f8e
Igor <ikkniazev@yandex.ru>
2021-11-03 11:56:36 +0300

    HOME_3D

--- client/src/components/AR3D/AR3D.jsx
+++ client/src/components/AR3D/AR3D.jsx
@@ -1,6 +1,18 @@
+// 3D
 import ReactThreeFbxViewer from 'react-three-fbx-viewer'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls, Stars } from "@react-three/drei"
 
 import styles from './AR3D.module.css'
+
+function Box() {
+  return (
+    <mesh>
+      <boxBufferGeometry url='./AR/russia.fbx' attach="geometry" />
+      <meshLambertMaterial attach="material" color="hotpink" />
+    </mesh>
+  )
+}
 
 function AR3D(props) {
   return (
@@ -9,16 +21,23 @@ function AR3D(props) {
         <div className={styles.ar_place_descr}>
           <p>ЗЕМЛЯ КРУГЛАЯ — НА КРАЯХ<br />ВСТРЕТИМСЯ, КРУТИ-ВЕРТИ!</p>
         </div>
-        {/* <div className={styles.ar_place_model}> */}
-          <ReactThreeFbxViewer cameraPosition={{
-            x: 20,
-            y: 100, // дальность
-            z: 20
-          }} url='./AR/russia.fbx' backgroundColor='#dcdcdc' near={3} far={1000} />
-        {/* </div> */}
+        <div className={styles.ar_place_model}> 
+          <Canvas>
+            <OrbitControls />
+            <Stars />
+            <ambientLight intensity={0.5} />
+            <spotLight position={[10, 15, 10]} angle={0.3} />
+            <Box />
+          </Canvas>
+        </div>
       </div>
     </>
   );
 }
 
 export default AR3D;
+{/* <ReactThreeFbxViewer cameraPosition={{
+            x:150,
+            y:300,
+            z:350,
+          }} url='./AR/russia.fbx' backgroundColor='#dcdcdc' near={3} far={1000} />  */}
