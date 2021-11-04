41bf724479b34aa2ded968870399aa3fed354b23
Igor <ikkniazev@yandex.ru>
2021-11-04 10:58:17 +0300

    EXAMPLE_NAV_POSITION

--- client/src/components/GameField/GameNav.jsx
+++ client/src/components/GameField/GameNav.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
+
 import Left from './Buttons/Left';
 import Down from './Buttons/Down'
 import Right from './Buttons/Right'
 import Refresh from './Buttons/Refresh'
 
+import styles from './GameField.module.css'
+
+
 function GameNav({ moveRight, moveLeft, rotateRightHandler, rotateLeftHandler, handlerRefresh }) {
   return (
-    <div>
+    <div className={styles.controller_block}> 
+      <Refresh handlerRefresh={handlerRefresh}/>
       <Left moveLeft={moveLeft} rotateLeftHandler={rotateLeftHandler} />
       <Down rotateRightHandler={rotateRightHandler}/>
       <Right moveRight={moveRight} />
-      <Refresh handlerRefresh={handlerRefresh}/>
     </div>
   );
 }
