63d652231a153cbaa8700ee2fb151d41faa1db0a
Christina <belyakovachristina@rambler.ruwd>
2021-11-05 11:09:44 +0300

    VIDEO_QR_PHOTO

--- client/src/components/PhotoBlock/PhotoBlock.jsx
+++ client/src/components/PhotoBlock/PhotoBlock.jsx
@@ -8,24 +8,24 @@ function PhotoBlock(props) {
     <>
     <div className={styles.peopleBlock}>
       <div className={styles.card}>
-        <div className={styles.card_img}></div>
+        <div className={styles.card_img}><img style={{  width: '353px', height: '351px' }} src="../../../crew/s.jpg" alt="" /></div>
         <p className={styles.card_text}>Сережа Клещев<br />Всему голова</p>
       </div>
       <div className={styles.card}>
-        <div className={styles.card_img}></div>
+        <div className={styles.card_img}><img style={{  width: '353px', height: '351px' }} src="../../../crew/a.jpg" alt="" /></div>
         <p className={styles.card_text}>Адель Галимова<br />зачинатель всея своё</p>
       </div>
       <div className={styles.card}>
-        <div className={styles.card_img}></div>
+        <div className={styles.card_img}><img style={{  width: '353px', height: '351px' }} src="../../../crew/k.jpg" alt="" /></div>
         <p className={styles.card_text}>Катя Курмышкина<br />зачинатель всея своё</p>
       </div>
       <div className={styles.card}>
-        <div className={styles.card_img}></div>
+        <div className={styles.card_img}><img style={{  width: '353px', height: '351px' }} src="../../../crew/m.jpg" alt="" /></div>
         <p className={styles.card_text}>Маша Козловцева<br />зачинатель всея своё</p>
       </div>
     </div>
     <div className={styles.big_card}>
-      <div className={styles.big_card_img}></div>
+      <div className={styles.big_card_img}><img style={{  width: '1855px', height: '918px' }} src="../../../crew/56_crew.jpg" alt="" /></div>
       <div className={styles.block_text}>
         <p className={styles.big_card_text}>Содружество своё!</p>
       </div>
